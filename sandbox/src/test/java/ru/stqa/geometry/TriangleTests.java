d4f19c0821a53b270da83686d338a6cc881592d5
ashtrayh <ashhttrayh@gmail.com>
2025-11-21 16:11:11 +0300

    Домашнее задание №4

--- sandbox/src/test/java/ru/stqa/geometry/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/TriangleTests.java
@@ -6,37 +6,40 @@ import org.junit.jupiter.api.Test;
 public class TriangleTests {
     @Test
     void canCalculatePerimeter() {
-        var s = new Triangle(3,5,6);
+        var s = new Triangle(3, 5, 6);
         Assertions.assertEquals(14, s.perimeter());
     }
+
     @Test
-void canCalculateArea() {
-        var s = new Triangle(6,8,10);
+    void canCalculateArea() {
+        var s = new Triangle(6, 8, 10);
         Assertions.assertEquals(24, s.area());
-}
-@Test
-    void cannotCreateTriangleWithNegativeSide() {
-    try {
-        new Triangle(-5, 7, 8);
-        Assertions.fail();
-    } catch (IllegalArgumentException exception) {
-        //OK
     }
-    try {
-        new Triangle(5, -7, 8);
-        Assertions.fail();
-    } catch (IllegalArgumentException exception) {
-        //OK
-    }
-    try {
-        new Triangle(5, 7, -8);
-        Assertions.fail();
-    } catch (IllegalArgumentException exception) {
-        //OK
-    }
-}
+
     @Test
-            void cannotCreateTriangleIfInequalityIsViolated() {
+    void cannotCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(-5, 7, 8);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //OK
+        }
+        try {
+            new Triangle(5, -7, 8);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //OK
+        }
+        try {
+            new Triangle(5, 7, -8);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //OK
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleIfInequalityIsViolated() {
         try {
             new Triangle(15, 7, 7);
             Assertions.fail();
@@ -55,6 +58,15 @@ void canCalculateArea() {
         } catch (IllegalArgumentException exception) {
             //OK
         }
+    }
+    @Test
+    void testEquality() {
+        var t1 = new Triangle(2, 3 , 4);
+        var t2 = new Triangle(4, 2 , 3);
+        var t3 = new Triangle(3, 4 , 2);
+        Assertions.assertEquals(t1, t2);
+        Assertions.assertEquals(t2, t3);
+        Assertions.assertEquals(t1, t3);
     }
 }
 
