eed9e5e124a370b57bd43e1f53a0dd822deedf71
ashtrayh <ashhttrayh@gmail.com>
2025-11-21 15:44:08 +0300

    Домашнее задание №3

--- sandbox/src/main/java/ru/stqa/geometry/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/Triangle.java
@@ -1,6 +1,16 @@
 package ru.stqa.geometry;
 
-public record Triangle(double a, double b, double c) {
+public record Triangle(double a, double b, double c)  {
+
+    public Triangle {
+    if (a < 0 || b < 0 || c < 0) {
+        throw new IllegalArgumentException("Triangle side should be non-negative");
+    }
+    if ( (a > b+c) || (b > a+c) || (c > a+b)) {
+        throw new IllegalArgumentException("The triangle inequality is violated");
+    }
+}
+
     public double perimeter() {
         return a + b + c;
     }
--- sandbox/src/test/java/ru/stqa/geometry/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/TriangleTests.java
@@ -6,12 +6,55 @@ import org.junit.jupiter.api.Test;
 public class TriangleTests {
     @Test
     void canCalculatePerimeter() {
-        var s = new Triangle(3,5,10);
-        Assertions.assertEquals(18, s.perimeter());
+        var s = new Triangle(3,5,6);
+        Assertions.assertEquals(14, s.perimeter());
     }
     @Test
 void canCalculateArea() {
         var s = new Triangle(6,8,10);
         Assertions.assertEquals(24, s.area());
 }
+@Test
+    void cannotCreateTriangleWithNegativeSide() {
+    try {
+        new Triangle(-5, 7, 8);
+        Assertions.fail();
+    } catch (IllegalArgumentException exception) {
+        //OK
+    }
+    try {
+        new Triangle(5, -7, 8);
+        Assertions.fail();
+    } catch (IllegalArgumentException exception) {
+        //OK
+    }
+    try {
+        new Triangle(5, 7, -8);
+        Assertions.fail();
+    } catch (IllegalArgumentException exception) {
+        //OK
+    }
 }
+    @Test
+            void cannotCreateTriangleIfInequalityIsViolated() {
+        try {
+            new Triangle(15, 7, 7);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //OK
+        }
+        try {
+            new Triangle(5, 15, 7);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //OK
+        }
+        try {
+            new Triangle(5, 7, 17);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+            //OK
+        }
+    }
+}
+
