a18758db0aebed2f5e259a3e285056d323ce4a99
ashtrayh <ashhttrayh@gmail.com>
2025-12-09 19:59:01 +0300

    Добавлен общий тест для одного контакта

--- addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
@@ -56,4 +56,33 @@ public class ContactInfoTests extends TestBase {
         Assertions.assertEquals(expected, emails);
     }
 
+    @Test
+    void testContactInfo() {
+        if (app.hbm().getContactCount() == 0) {
+            app.hbm().createContact(new ContactData("", "firstname", "lastname", "email@gmail.com", "", "+78954512", "+95456311", "+65522363", "+65363552", "Tverskaya,36", "email2@gmail.com", "email3@gmail.com"));
+            app.contacts().reloadHomePage();
+        }
+        var contacts = app.hbm().getContactList();
+        var expectedPhones = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
+                        .filter(s -> s != null && !s.isEmpty())
+                        .collect(Collectors.joining("\n"))
+        ));
+        var phones = app.contacts().getPhones();
+        Assertions.assertEquals(expectedPhones, phones);
+        var expectedEmails = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.email(), contact.email2(), contact.email3())
+                        .filter(s -> s != null && !s.isEmpty())
+                        .collect(Collectors.joining("\n"))
+        ));
+        var emails = app.contacts().getEmails();
+        Assertions.assertEquals(expectedEmails, emails);
+        var expectedAddress = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.address())
+                        .filter(s -> s != null && !s.isEmpty())
+                        .collect(Collectors.joining("\n"))
+        ));
+        var address = app.contacts().getAddress();
+        Assertions.assertEquals(expectedAddress, address);
+    }
 }
