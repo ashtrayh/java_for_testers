022df344c8d1768133b054d98d5fb902b34728e1
ashtrayh <ashhttrayh@gmail.com>
2025-12-22 17:33:10 +0300

    Лекция 8.3

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/LoginTests.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 public class LoginTests extends TestBase {
     @Test
     void canLogin() {
-        app.session().login("administrator", "root");
-        Assertions.assertTrue(app.session().isLoggedIn());
+        app.http().login("administrator", "root");
+        Assertions.assertTrue(app.http().isLoggedIn());
     }
+
+
 }
